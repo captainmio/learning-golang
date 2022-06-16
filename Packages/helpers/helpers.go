f162a5ec583338da9faa6933e5faab3d80885bd3
Rustum Jordan <rustum@thinksmartsoftware.com>
2022-06-16 21:38:38 +0800

    tutorial for Channels and read & write json

--- Packages/helpers/helpers.go
+++ Packages/helpers/helpers.go
@@ -1,10 +1,18 @@
 package helpers
 
+import (
+	"math/rand"
+	"time"
+)
+
 type SomeType struct {
 	TypeName   string
 	TypeNumber int
 }
 
-func main() {
+func RandomNumber(n int) int {
+	rand.Seed(time.Now().UnixNano())
+	value := rand.Intn(n)
 
+	return value
 }
--- Packages/main.go
+++ Packages/main.go
@@ -6,6 +6,11 @@ import (
 	"github.com/captainmio/samplePackage/helpers"
 )
 
+func CalculateValue(intChan chan int) {
+	randomNumber := helpers.RandomNumber(100)
+	intChan <- randomNumber
+}
+
 func main() {
 	log.Println("Sample Package")
 	var myVar helpers.SomeType
@@ -13,6 +18,15 @@ func main() {
 	myVar.TypeName = "Something Naming"
 
 	log.Println(myVar.TypeName)
+
+	intChan := make(chan int)
+	defer close(intChan)
+
+	// this execute the function as a go routine
+	go CalculateValue(intChan)
+
+	num := <-intChan
+	log.Print(num)
 }
 
 // use the command "go mod init"; example command is go mod init github.com/captainmio/samplePackage
