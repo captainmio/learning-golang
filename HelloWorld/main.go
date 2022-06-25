dd7cfb722ae0e8f3a0d0008534f5e4647f4a6a82
Rustum Jordan <rustum@thinksmartsoftware.com>
2022-06-25 14:48:19 +0800

    implement go html template

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -10,41 +10,22 @@ const portNumber = ":8080"
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "this is the home page")
+	renderTemplate(w, "home.page.gohtml")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	mySum := addValues(2, 5)
-	fmt.Fprintf(w, fmt.Sprintf("this is the about page and 2 + 5 is %d", mySum))
+	renderTemplate(w, "about.page.gohtml")
 }
 
-// addValues is a method to calculate 2 integer and returns the sum
-func addValues(x int, y int) int {
-	return x + y
-}
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	var x float64 = 100.0
-	var y float64 = 0.0
-	f, err := divideValues(x, y)
+func renderTemplate(w http.ResponseWriter, gohtml string) {
+	parseTemplate, _ := template.ParseFiles("./templates/" + gohtml)
+	err := parseTemplate.Execute(w, nil)
 
 	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by 0")
-	} else {
-		fmt.Fprintf(w, fmt.Sprintf("%f divide by %f is %f", x, y, f))
+		fmt.Println("error parsing template:", err)
+		return
 	}
-
-}
-
-func divideValues(x, y float64) (float64, error) {
-	if y <= 0 {
-		err := errors.New("cannot divide by zero")
-		return 0, err
-	}
-	var result float64
-	result = x / y
-	return result, nil
 }
 
 // main is the entry point
@@ -53,7 +34,6 @@ func main() {
 	// routes
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Start running the application in port %s", portNumber))
 	http.ListenAndServe(portNumber, nil)
